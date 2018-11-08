81e2fcdee19a86c0f69be7054762ffe5d9cad20f
Aportillo1 <alan.9612@gmail.com>
2018-11-08 09:28:00 -0800

    first commit

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -17,45 +17,57 @@ export class MainContentComponent implements OnInit {
 
   students: Array<IStudent> = [];
 
+  editMode: Boolean = false;
+
   constructor() {
     this.students[0] = {
       id: 1,
       firstName: 'Lucas',
       lastName: 'Phan',
       course: 'Programming'
-    }
+    };
     this.students[1] = {
       id: 1,
       firstName: 'Tom',
       lastName: 'Brady',
       course: 'Swimming'
-    }
+    };
     this.students[2] = {
       id: 1,
       firstName: 'Mark',
       lastName: 'Zuc',
       course: 'Business'
-    }
+    };
     this.students[3] = {
       id: 1,
       firstName: 'Joe',
       lastName: 'Biden',
       course: 'Politics'
-    }
+    };
   }
 
   ngOnInit() {
   }
 
   addStudent() {
+    this.editMode = true;
     const student: IStudent = {
-      id: 1,
-      firstName: 'Mike',
-      lastName: 'Tyson',
-      course: 'Boxing'
+      id: null,
+      firstName: null,
+      lastName: null,
+      course: null
     };
 
-    this.students.push(student);
+    // this.students.push(student);
+    this.students.unshift(student);
   }
 
+  removeStudent(index: number) {
+    console.log('from removeStudent function index: ', index);
+    this.students.splice(index, 1);
+  }
+
+  saveStudent() {
+    this.editMode = false;
+  }
 }
